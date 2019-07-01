15b2a0b04c198df5db0602c902dce4a060e548eb
Logan Emerson <logemerson@gmail.com>
2019-07-01 18:18:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ const returnLastTwoDrivers = (function(drivers) {
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-const createFareMultiplier = 2;
+const createFareMultiplier = (function(foo) {
+  return foo;
+});
